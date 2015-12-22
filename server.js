6c7217807cc35850ca05a84115072475d3daabb9
maukka76 <markus.veijola@gmail.com>
2015-12-22 12:26:45 +0200

    codes

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 var express = require('express');
+var mongoose = require('mongoose');
 
 var app = express();
+
+//Configure mongodb
+var db_name = 'template';
+mongodb_connection_string = 'mongodb://127.0.0.1:27017/' + db_name;
+
+//take advantage of openshift env vars when available:
+if(process.env.OPENSHIFT_MONGODB_DB_URL){
+  mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + db_name;
+}
+
 
 //You need to define these two variables with these
 //two environment variables to get you app work in openshift
@@ -10,7 +21,17 @@ var ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 //When this server is connected, return the index.html as response
 app.get('/',function(req,res){
     
-    res.sendfile('index.html');
+    //Now connect with configuration
+    mongoose.connect(mongodb_connection_string, function(err,pass){
+
+        if(err){
+            res.send(err.message);
+        }
+        else{
+
+            res.send('Your dataabse is running in:' + mongodb_connection_string);
+        }
+    });
 });
 
 
